c8303c68db841919aef9c440167b7ccaff00164a
Gom3a <kkrem551@gmail.com>
2019-02-23 16:07:00 +0200

    Redesign/Reidea for the project

--- home.php
+++ home.php
@@ -10,21 +10,32 @@
 	<div class="About">
 
 		<div class="content">
-			<h1>Traveling</h1>
+			<h1>Movie World</h1>
 			<p>
 
-				Hers is your place to know every thing about some place 
-				with us you can travel to anywhere what you want from your home
+				Hers is your place to know every thing about some movie
+				with us you can search for any thing related to any movie in Cenema World :D
 
 			</p>
 			
 				<div class="container">
-						<form action="" class="form" method="post">
-							<input type="text" name="city" class="TextField" placeholder="some place">
+						<form action="" class="form" method="get">
+							<input type="text" name="mname" class="TextField" placeholder="Movie Name">
 							<input type="submit" name="submit" class="btn" value="Search">
 						</form>
 				</div>
-					
+				<div class="container">
+						<form action="" class="form" method="get">
+							<input type="text" name="actor" class="TextField" placeholder="Actor">
+							<input type="submit" name="submit" class="btn" value="Search">
+						</form>
+				</div>
+				<div class="container">
+						<form action="" class="form" method="get">
+							<input type="text" name="cat" class="TextField" placeholder="Category">
+							<input type="submit" name="submit" class="btn" value="Search">
+						</form>
+				</div>
 		</div>
 	</div>
 
